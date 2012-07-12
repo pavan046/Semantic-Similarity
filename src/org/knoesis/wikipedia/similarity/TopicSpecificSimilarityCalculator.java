59bd97d27cba0b1bb222c79d5aa63f77dc133ce3
Pramod Koneru <pramodkoneru549@gmail.com>
2012-07-12 12:11:02 -0400

    Committing topic specific similarity

--- src/org/knoesis/wikipedia/similarity/TopicSpecificSimilarityCalculator.java
+++ src/org/knoesis/wikipedia/similarity/TopicSpecificSimilarityCalculator.java
@@ -8,7 +8,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.knoesis.wikipedia.api.WikipediaParser;
+import org.openjena.atlas.logging.Log;
 
 /**
  * This class generates a list of entities with weights 
@@ -20,6 +22,8 @@ import org.knoesis.wikipedia.api.WikipediaParser;
  * 		 Later this has to be changed to provide for more hops. 
  * 		
  * TODO: Also the scalability should be considered. Transform the problem to Map Reduce.
+ * 
+ * FIXME: Use logging everytime you code.
  * @author pavan
  *
  */
@@ -27,6 +31,8 @@ public class TopicSpecificSimilarityCalculator {
 	private static String wikipediaTopic;
 	private static int hops;
 	private WikipediaParser parser;
+	private static Logger logger = Logger.getLogger(TopicSpecificSimilarityCalculator.class);
+
 	/**
 	 *  
 	 * @param wikipediaTopic
@@ -37,7 +43,7 @@ public class TopicSpecificSimilarityCalculator {
 		this.hops = hops;
 		parser = new WikipediaParser(wikipediaTopic);
 	}
-	
+
 	/**
 	 * 
 	 * @param wikipediaTopic
@@ -58,16 +64,21 @@ public class TopicSpecificSimilarityCalculator {
 	public Map<String, Integer> calculate(){
 		List<String> firstLinks = parser.getLinks();
 		Map<String, List<String>> secondLinks = new HashMap<String, List<String>>();
-		
+
 		for(String link: firstLinks){
-			parser.setWikipediaPage(link);
-			secondLinks.put(link, parser.getLinks());
+			// FIXME: Find a better way to do this, as of now just replacing Templates 
+			if(!link.contains("Category") || !link.contains("Wikipedia")){
+				logger.info("processing "+link+" article");
+				String linkWithoutTemplates = link.replace("Template:", "");
+				parser.setWikipediaPage(linkWithoutTemplates);
+				secondLinks.put(linkWithoutTemplates, parser.getLinks());
+			}
 		}
 		//System.out.println(secondLinks);
-			
+
 		return calculateWeights(secondLinks);
 	}
-	
+
 	private Map<String, Integer> calculateWeights(Map<String, List<String>> secondLinks){
 		Map<String, Integer> results = new HashMap<String, Integer>();
 		for(String firstLink: secondLinks.keySet()){
@@ -88,18 +99,21 @@ public class TopicSpecificSimilarityCalculator {
 		}
 		return results;
 	}
-	
+
 	public static void main(String[] args) {
+		logger.info("Starting the process for US Elections 2012 " + System.currentTimeMillis());
+				
 		try {
-			Writer write = new FileWriter(new File("us_elections_links.ranking"));
+			Writer write = new FileWriter(new File("obamacare_links.ranking_28th"));
 			//United_States_presidential_election,_2012
-			TopicSpecificSimilarityCalculator wikiCalc = new TopicSpecificSimilarityCalculator("United_States_presidential_election,_2012");
+			TopicSpecificSimilarityCalculator wikiCalc = new TopicSpecificSimilarityCalculator("Patient Protection and Affordable Care Act");
 			Map<String, Integer> relatedLinks = wikiCalc.calculate();
 			for(String link: relatedLinks.keySet()){
 				write.append(link+"\t"+relatedLinks.get(link)+"\n");
 			}
 			write.close();
-			
+		logger.info("Ending the process for US Elections 2012 " + System.currentTimeMillis());
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
