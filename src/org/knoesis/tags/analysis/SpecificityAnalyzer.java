63851d75cbd918ee709ffcf905bbaff75d7878b2
Pramod Koneru <pramodkoneru549@gmail.com>
2012-07-23 18:14:57 -0400

    Implemented analyse method to get the specificity measure.

--- src/org/knoesis/tags/analysis/SpecificityAnalyzer.java
+++ src/org/knoesis/tags/analysis/SpecificityAnalyzer.java
@@ -1,8 +1,15 @@
 package org.knoesis.tags.analysis;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.knoesis.models.AnnotatedTweet;
+import org.knoesis.similarity.CosineSimilarityCalculator;
+import org.knoesis.twarql.extractions.Extractor;
+import org.knoesis.twarql.extractions.TagExtractor;
+import org.knoesis.twarql.extractions.TermFrequencyGenerator;
+import org.knoesis.twitter.crawler.SearchTwitter;
 /**
  * This class analyzes the specificity of a hashtag by analyzing
  * the extent to which the usage of a hashtag deviates from the 
@@ -20,15 +27,44 @@ import org.knoesis.models.AnnotatedTweet;
 public class SpecificityAnalyzer implements Analyzer {
 	private List<AnnotatedTweet> tweetsOfHashtag;
 	private List<AnnotatedTweet> tweetsOfKeyword;
+	private double specificityMeasure;
+	
+	private TermFrequencyGenerator termFreqGenerator = null;
+	
+	private Map<String, Integer> termFreqForTweetsOfHashtag = null;
+	private Map<String, Integer> termFreqForTweetsOfKeyword = null;
 	
 	public SpecificityAnalyzer(List<AnnotatedTweet> tweetsOfHashtag, List<AnnotatedTweet> tweetsOfKeyword) {
 		this.tweetsOfHashtag = tweetsOfHashtag;
 		this.tweetsOfKeyword = tweetsOfKeyword;
+		termFreqGenerator = new TermFrequencyGenerator();
 	}
 	
 	@Override
 	public void analyze() {
-		
+		termFreqForTweetsOfHashtag = termFreqGenerator.extractListTweets(tweetsOfHashtag);
+		termFreqForTweetsOfKeyword = termFreqGenerator.extractListTweets(tweetsOfKeyword);		
+		specificityMeasure =  CosineSimilarityCalculator.calculate(termFreqForTweetsOfHashtag, termFreqForTweetsOfKeyword);				
 	}
 
+	public void setSpecificityMeasure(double specificityMeasure) {
+		this.specificityMeasure = specificityMeasure;
+	}
+
+	public double getSpecificityMeasure() {
+		return specificityMeasure;
+	}
+
+	public static void main(String[] args) {
+		List<Extractor> extractors = new ArrayList<Extractor>();
+		extractors.add(new TagExtractor());
+		SearchTwitter searchTwitter = new SearchTwitter(extractors);
+		// This will get the last 1500 annotated Tweets.
+		List<AnnotatedTweet> tweetsOfHashtag = searchTwitter.getTweets("#gop", true);
+		List<AnnotatedTweet> tweetsOfKeyword = searchTwitter.getTweets("gop", false);
+		
+		SpecificityAnalyzer specificity = new SpecificityAnalyzer(tweetsOfHashtag, tweetsOfKeyword);
+		specificity.analyze();
+		System.out.println("The specificity Measure is :" + specificity.getSpecificityMeasure());
+	}
 }
